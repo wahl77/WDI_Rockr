95f6f20dce77b613a4063cc220106ce3c66c9495
Sumeet Jain <sumeet@sumeetjain.com>
2013-07-30 07:01:00 -0700

    rails new wdi-rockr -d postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WdiRockr::Application.config.secret_token = 'accf2f3d10ab3b3c6877d0dbc355bc590e92a2b4605fc56ae4b92023c967132f9678a3bfa9272f11c4fae40db298559bd530ebdf9af505d552752efe46e0bea8'
